a5eca42698aac378a75f3d4f74c30f247edb1ac0
Dan Marsden <dan@danmarsden.com>
2024-06-04 20:21:44 +1200

    Update version.php

--- version.php
+++ version.php
@@ -23,8 +23,8 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2024042201;
-$plugin->release = 2024042201;
+$plugin->version  = 2024042202;
+$plugin->release = 2024042202;
 $plugin->requires = 2023100900; // Requires 4.3.
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->cron     = 0;
